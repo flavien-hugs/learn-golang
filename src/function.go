91e6f445c68179f8dce281cd0c3c2ef210455722
Flavien HUGS <flavienhugs@pm.me>
2024-07-12 23:00:13 +0000

    Add fonction example

--- src/function.go
+++ src/function.go
@@ -1,1 +1,25 @@
+// anonyme function in go
+
 package main
+
+import "fmt"
+
+func main() {
+	// Fonction anonyme ou fermeture: Ce sont des fonctions qui ne necissite pas d'être nommer.
+	// Elle est dite fonction définie inline
+
+	funcAnonym := func() {
+		fmt.Println("Hello, je suis une fonction anonyme !")
+	}
+
+	funcAnonym()
+
+	// example:
+	addition := func(x int, y int) int {
+		return x + y
+	}
+
+	for n := 0; n < 3; n++ {
+		fmt.Println("result --> ", addition(10, n))
+	}
+}
